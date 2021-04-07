5639c7d69ef2d048827b855436058837c4d98d64
Hanif Maleki <hanif_1362@yahoo.com>
2021-04-07 23:38:53 +0200

    AF-5 Stage-1 structure #time 1h #done

---
+++ stage-1/src/test/kotlin/org/hyperskill/spring/fraud/antifraud/FraudDetectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.hyperskill.spring.fraud.antifraud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FraudDetectionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
