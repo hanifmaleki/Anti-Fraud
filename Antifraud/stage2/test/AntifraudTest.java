29247ecfd58c79c8a38686a03994ce80d59bab4b
Hanif Maleki <hanif_1362@yahoo.com>
2021-04-25 23:38:46 +0200

    AF-14 Storage-2 tests #time 90m #done

--- Antifraud/stage2/test/AntifraudTest.java
+++ Antifraud/stage2/test/AntifraudTest.java
@@ -8,10 +8,11 @@ import org.hyperskill.hstest.mocks.web.response.HttpResponse;
 import org.hyperskill.hstest.stage.SpringTest;
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.springframework.http.HttpStatus;
-import org.springframework.lang.Nullable;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 
 public class AntifraudTest extends SpringTest {
@@ -20,6 +21,9 @@ public class AntifraudTest extends SpringTest {
 
     private final User user1;
     private final User user2;
+    private final User userWithoutRole;
+    private final User userWithoutName;
+    private final User userWithoutUsername;
 
     private final Gson gson = new Gson();
 
@@ -38,94 +42,152 @@ public class AntifraudTest extends SpringTest {
                 .username("richard_roe")
                 .role(Role.ADMIN)
                 .build();
+
+        userWithoutRole = User
+                .builder()
+                .name("John Doe")
+                .username("john_doe")
+                .build();
+
+        userWithoutName = User
+                .builder()
+                .username("john_doe")
+                .role(Role.USER)
+                .build();
+
+        userWithoutUsername = User
+                .builder()
+                .username("john_doe")
+                .role(Role.USER)
+                .build();
     }
 
 
     @DynamicTest
+        // Normal Behaviour
     CheckResult testUser1() {
+        try {
+            test1MainScenario();
+            return CheckResult.correct();
+        } catch (UnexpectedResultException exception) {
+            return exception.getResult();
+        }
+    }
+
+    @DynamicTest
+        // Adding incomplete users and expect 209
+    CheckResult testUser2() {
+        try {
+            test2MainScenario();
+            return CheckResult.correct();
+        } catch (UnexpectedResultException exception) {
+            return exception.getResult();
+        }
+    }
+
+    private void test1MainScenario() {
         // Add User1 and expect 200
-        addUSerAndExceptSuccess(user1);
+        addUserAndExceptStatus(user1, OK);
 
         // Get Users
         List<User> users = getUsersAndExpectSize(1);
 
 
         // Check The only element is user1
-        User receivedUser = users.get(0);
+        checkUserExistingList(users, user1);
+
+        // Add User2 and expect 200
+        addUserAndExceptStatus(user2, OK);
+
+
+        // Get Users (User1, User2)
+        users = getUsersAndExpectSize(2);
+        checkUserExistingList(users, user2);
+
+        // Delete User1
+        deleteExistingUser(user1.getUsername());
+
+        // Get Users
+        users = getUsersAndExpectSize(1);
+        checkUserExistingList(users, user2);
+        // Delete User2
+        deleteExistingUser(user2.getUsername());
+
+        // Get Epty List
+        getUsersAndExpectSize(0);
+
+    }
+
+    private void test2MainScenario() {
+        // Add User1_Incomplete1
+        addUserAndExceptStatus(userWithoutName, UNPROCESSABLE_ENTITY);
+        addUserAndExceptStatus(userWithoutRole, UNPROCESSABLE_ENTITY);
+        addUserAndExceptStatus(userWithoutUsername, UNPROCESSABLE_ENTITY);
+
+
+        // GET Empty List
+        getUsersAndExpectSize(0);
+
+        addUserAndExceptStatus(user1, OK);
+        addUserAndExceptStatus(user1, CONFLICT);
+
+    }
+
+    private void test3MainScenario() {
+
+        // Add Use1
+        addUserAndExceptStatus(user1, OK);
+
+        // Add User2
+        addUserAndExceptStatus(user2, OK);
+
+        // Delete Username3
+        //Except Error_Code
+        deleteNonExistingUser("username3");
+
+    }
+
+
+    private void checkUserExistingList(List<User> users, User user) {
+        User receivedUser = users.stream().filter(user::equals).findFirst().orElseThrow(() -> {
+            String feedback = String.format("The expected user %s does nt exist in the received list", user1);
+            return new UnexpectedResultException(CheckResult.wrong(feedback));
+        });
+
         boolean isDifferent =
-                !user1.getName().equals(receivedUser.getName()) ||
-                        !user1.getUsername().equals(receivedUser.getUsername()) ||
-                        user1.getRole() != receivedUser.getRole();
+                !user.getName().equals(receivedUser.getName()) ||
+                        !user.getUsername().equals(receivedUser.getUsername()) ||
+                        user.getRole() != receivedUser.getRole();
 
         if (isDifferent) {
             String feedback = String.format(
                     "The received user is not matched with created user.\n Get: %s \n Expected: %s",
                     receivedUser,
-                    user1
+                    user
             );
-            return CheckResult.wrong(feedback);
+            throw new UnexpectedResultException(CheckResult.wrong(feedback));
         }
-
-
-        // Add User1 and expect 200
-        addUSerAndExceptSuccess(user2);
-
-
-        // Get Users (User1, User2)
-        users = getUsersAndExpectSize(2);
-        //TODO check user
-
-        // Delete User1
-        // Get User2
-        // Delete User2
-        // Get EMpty List
-        return null;
     }
 
-    @DynamicTest
-    CheckResult testUser2() {
-        // Add User1_Incomplete1
-        //Except Error-Code
-        // Add User2_Incomplete2
-        //Except Error_Code
-        // GET Empty List
-        // Add Use1
-        // Add User2
-        // Delete Username3
-        //Except Error_Code
-
-        return null;
-
+    private void deleteExistingUser(String username) {
+        HttpRequest delete = delete(address + "/" + username);
+        HttpResponse response = delete.send();
+        if (response.getStatusCode() != OK.value()) {
+            String feedback = String.format("Could not delete existing user %s", username);
+            throw new UnexpectedResultException(CheckResult.wrong(feedback));
+        }
     }
 
-    private CheckResult callMethodAndCheckResult(@Nullable Long amount, String expectedResult, Object message) {
-
-        HttpResponse response = getHttpResponse(amount);
-
-
-        if (response.getStatusCode() != HttpStatus.OK.value()) {
-            String feedback = String.format("GET %s should respond with status code 200, responded: %d\n\n" +
-                            "Response body:\n%s"
-                    , address, response.getStatusCode(), response.getContent());
-            return CheckResult.wrong(feedback);
+    private void deleteNonExistingUser(String username) {
+        HttpRequest delete = delete(address + "/" + username);
+        HttpResponse response = delete.send();
+        int statusCode = response.getStatusCode();
+        int expectedValue = NOT_FOUND.value();
+        if (statusCode != expectedValue) {
+            String feedback = String.format("Not existing user %s deletion should return code %d and not %d"
+                    , username, expectedValue, statusCode);
+            throw new UnexpectedResultException(CheckResult.wrong(feedback));
         }
-
-        String result = response.getContent().toLowerCase().trim();
-        if (!result.contains(expectedResult)) {
-            String feedback = String.format("The expected result is %s, your result is %s. %s", expectedResult, result, message);
-            return CheckResult.wrong(feedback);
-        }
-
-        return CheckResult.correct();
-    }
-
-    private HttpResponse getHttpResponse(Long amount) {
-        HttpRequest httpRequest = get(address);
-        if (amount != null) {
-            String parameter = String.valueOf(amount);
-            httpRequest.addParam("amount", parameter);
-        }
-        return httpRequest.send();
     }
 
     private HttpResponse addUser(User user) {
@@ -134,15 +196,15 @@ public class AntifraudTest extends SpringTest {
         return httpRequest.send();
     }
 
-
-    private void addUSerAndExceptSuccess(User user) {
-        // Add User1
+    private void addUserAndExceptStatus(User user, HttpStatus httpStatus) {
+        // Add User
         HttpResponse response = this.addUser(user);
-        // Except code 200
-        if (response.getStatusCode() != HttpStatus.OK.value()) {
-            String feedback = String.format("POST %s should respond with status code 200, responded: %d\n\n" +
+        // Except code 422
+        int status = httpStatus.value();
+        if (response.getStatusCode() != status) {
+            String feedback = String.format("POST %s should respond with status code %d, responded: %d\n\n" +
                             "Response body:\n%s"
-                    , address, response.getStatusCode(), response.getContent());
+                    , address, status, response.getStatusCode(), response.getContent());
             CheckResult result = CheckResult.wrong(feedback);
 
             throw new UnexpectedResultException(result);
@@ -157,7 +219,7 @@ public class AntifraudTest extends SpringTest {
         HttpRequest httpRequest = get(address);
         HttpResponse response = httpRequest.send();
 
-        if (response.getStatusCode() != HttpStatus.OK.value()) {
+        if (response.getStatusCode() != OK.value()) {
             String feedback = String.format("GET %s should respond with status code 200, responded: %d\n\n" +
                             "Response body:\n%s"
                     , address, response.getStatusCode(), response.getContent());
