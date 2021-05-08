0d8b44da740ade849401a2ee0c623ca56548f736
Hanif Maleki <hanif_1362@yahoo.com>
2021-05-08 11:34:54 +0200

    AF-21 Storage-3 tests #time 5m

---
+++ Antifraud/stage4/src/antifraud/model/User.java
@@ -0,0 +1,17 @@
+package antifraud.model;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @EqualsAndHashCode.Exclude
+    private String name;
+
+    private String username;
+
+    @EqualsAndHashCode.Exclude
+    private Role role;
+}
