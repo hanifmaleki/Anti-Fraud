ea5a23edcde4b3a21c44f2a075c966de40da1796
Hanif Maleki <hanif_1362@yahoo.com>
2021-05-08 14:07:23 +0200

    AF-29 Stage-3 Models and Interfaces #time 10m
    * Add TransactionResponse model

--- Antifraud/stage3/src/antifraud/controller/TransactionController.java
+++ Antifraud/stage3/src/antifraud/controller/TransactionController.java
@@ -2,6 +2,7 @@ package antifraud.controller;
 
 import antifraud.model.ResultEnum;
 import antifraud.model.Transaction;
+import antifraud.model.TransactionResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,17 +11,20 @@ import org.springframework.web.bind.annotation.*;
 public class TransactionController {
 
     @GetMapping
-    ResponseEntity<String> isTransactionValid(@RequestBody Transaction transaction) {
+    ResponseEntity<TransactionResponse> isTransactionValid(@RequestBody Transaction transaction) {
         int amount = transaction.getAmount();
 
+        ResultEnum result;
+
         if (amount <= 200L) {
-            return ResponseEntity.ok(ResultEnum.ALLOWED.name());
+//            return ResponseEntity.ok(ResultEnum.ALLOWED.name());
         }
 
         if (amount <= 1500L) {
-            return ResponseEntity.ok(ResultEnum.MANUAL_PROCESSING.name());
+//            return ResponseEntity.ok(ResultEnum.MANUAL_PROCESSING.name());
         }
-        return ResponseEntity.ok(ResultEnum.PROHIBITED.name());
+//        return ResponseEntity.ok(ResultEnum.PROHIBITED.name());
+        return null;
     }
 
 }
